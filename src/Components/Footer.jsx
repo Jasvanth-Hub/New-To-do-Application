73552a404c1012a942b91e84966446baf7160332
Jasvanth-Hub <bhukyajasvanth@gmail.com>
2025-02-13 13:38:23 +0530

    updated footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <>
             <div className="footer bg-slate-600 text-white text-center p-3 fixed w-[100%]  bottom-[0]">
-                <p>Developed by Jasvanth || Copy right &copy; || All rights received.</p>
+                <p>Developed by Jasvanth || Copy right &copy; 2024 || All rights reserved.</p>
             </div>
         </>
     )
