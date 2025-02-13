7d8bc4c4154a328553aacb818963828bdeefd25a
Jasvanth-Hub <bhukyajasvanth@gmail.com>
2025-02-13 23:12:14 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/New-To-do-Application/'
+  base: '',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/index.js`,
+        chunkFileNames: `assets/index-[hash].js`,
+        assetFileNames: `assets/index-[hash][extname]`,
+      },
+    },
+  },
 })
